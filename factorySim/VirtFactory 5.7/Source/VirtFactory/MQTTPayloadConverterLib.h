07849362c721e5836b9140133589905807f9dd2e
szupererik2 <72521839+szupererik2@users.noreply.github.com>
2025-12-19 03:21:51 +0200

    Dashing through the snoooow

---
+++ factorySim/VirtFactory 5.7/Source/VirtFactory/MQTTPayloadConverterLib.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MQTTPayloadConverterLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTFACTORY_API UMQTTPayloadConverterLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
